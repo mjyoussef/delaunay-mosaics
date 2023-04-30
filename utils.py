dd4448f4692d28e93715324c53dfa19b35ecca59
Michael Youssef <michael_youssef@brown.edu>
2023-04-30 15:47:53 -0400

    remove intersecting segments

--- utils.py
+++ utils.py
@@ -54,7 +54,11 @@ def dist(p1, p2):
 
 def orientation_of(p1, p2, p3):
     '''
-    computes the orientation of three points (-1 = cw, 0 = colinear, 1 = ccw)
+    computes the orientation of three points
+
+    p1, p2, p3: coordinates of points
+
+    returns -1 for cw, 0 for colinear, and 1 for ccw
     '''
     det_matrix = np.array([[p1[0], p1[1], 1], [p2[0], p2[1], 1], [p3[0], p3[1], 1]])
     det_sign = np.linalg.det(det_matrix)
@@ -68,7 +72,12 @@ def orientation_of(p1, p2, p3):
 
 def segments_intersect(e1, e2):
     '''
-    returns true if two edges with no shared endpoints intersect each other
+    determines if two edges intersect each other
+    ** Note: edges with a single matching endpoint do not intersect for our purposes **
+
+    e1, e2: edges
+
+    returns true/false
     '''
     if (e1[0] == e2[0] or e1[0] == e2[1] or e1[1] == e2[0] or e1[1] == e2[1]):
         return False
@@ -77,28 +86,154 @@ def segments_intersect(e1, e2):
     o_2 = orientation_of(e1[0], e1[1], e2[1])
 
     o_3 = orientation_of(e2[0], e2[1], e1[0])
-    o_4 = orientation_of(e2[0], e2[1], e1[0])
+    o_4 = orientation_of(e2[0], e2[1], e1[1])
 
-    if (o_1 == 0): # colinear
+    if (o_1 == 0 and o_2 == 0 and o_3 == 0 and o_4 == 0): # colinear
+
         x_projs = [(e1[0][0], 0), (e1[1][0], 0), (e2[0][0], 1), (e2[1][0], 1)]
         list.sort(x_projs, key=lambda x: x[0])
+
         y_projs = [(e1[0][1], 0), (e1[1][1], 0), (e2[0][1], 1), (e2[1][1], 1)]
         list.sort(y_projs, key=lambda y: y[0])
 
+        # check if there is overlap in the x, y projections of the segments
         return x_projs[0][1] != x_projs[1][1] or y_projs[0][1] != y_projs[0][1]
-        
-    return o_1 != o_2 and o_3 != o_4
     
+    # if points are not colinear, they intersect when
+    # the orientations 1,2 and 3,4 are different
+    return o_1 != o_2 and o_3 != o_4
+
+def filter_edges(edges, adj_mat):
+    '''
+    Helper function for finding a maximal independent subset in the adj_mat
+    ** Note: this function is attemtping to find something as close as possible to the 
+    maximum independent subset given that the adjaceny matrix tends to be very sparse **
+
+    edges: a list of edges
+    adj_mat: an adjacency matrix where the "nodes" are edges from the `edges` list
+    and an edge between two nodes exists if they intersect each other
+
+    returns a subset of edges from `edges` s.t that no edge intersects another edge
+    '''
+    n = len(edges)
+    
+    # counts the number of edges each edge neighbors in adj_mat
+    intersects = adj_mat.sum(axis=1)
+
+    # keep track of how many edges need to be removed
+    intersects_copy = intersects.copy()
+    intersects_copy[intersects_copy > 0] = 1
+    remaining = intersects_copy.sum()
+
+    # indices of edges that need to be removed
+    edges_to_remove = set()
+
+    while (remaining > 0):
+        # find edge with greatest number of neighbors
+        edge_idx = np.argmax(intersects)
+        edges_to_remove.add(edge_idx)
+
+        # iterate over neighbors, updating `remaining` along the way
+        for i in range(n):
+            if (adj_mat[edge_idx][i] == 1):
+                intersects[i] -= 1
+                if (intersects[i] == 0):
+                    remaining -= 1
+        
+        intersects[edge_idx] = 0
+        remaining -= 1
+    
+    new_edges = []
+    for i in range(n):
+        if (i not in edges_to_remove):
+            new_edges.append(edges[i])
+    
+    return new_edges
+
 def remove_intersecting_edges(edges):
     '''
     removes edges that intersect at anything other than end-points
-    '''
-    pass
 
-def remove_close_edges(edges):
+    Note: the goal of this procedure is to remove as few edges as possible
+    such that none of the edges in the new list intersect; this is impossible to do efficiently
+    since you can reduce maximum independent sets to this problem
     '''
-    removes edges that are too close to one another;
+    n = len(edges)
+    adj_mat = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(i):
+            if (segments_intersect(edges[i], edges[j])):
+                adj_mat[i][j] = 1
+                adj_mat[j][i] = 1
+    
+    # remove edge that intersects the greatest number of edges
+    return filter_edges(edges, adj_mat)
+
+def distance_less_thresh(e1, e2, dist_thresh, theta_thresh):
     '''
+    computes distance between two edges
+
+    Note: this is a custom distance function that considers two edges
+    as being close if the distance between them is below a threshold AND
+    the angular distance between them is below a threshold
+    '''
+
+    x1, y1 = e1
+    x2, y2 = e2
+    ep_dist = min(dist(x1, x2), dist(x1, y2), dist(y1, x2), dist(y1, y2))
+
+    if (ep_dist >= dist_thresh):
+        return False
+    
+    v1, v2 = None, None
+    if (ep_dist == dist(x1, x2)):
+        y1[0] -= x1[0]
+        y1[1] -= x1[1]
+        y2[0] -= x2[0]
+        y2[1] -= x2[1]
+        v1, v2 = y1, y2
+    elif (ep_dist == dist(x1, y2)):
+        y1[0] -= x1[0]
+        y1[1] -= x1[1]
+        x2[0] -= y2[0]
+        x2[1] -= y2[1]
+        v1, v2 = y1, x2
+    elif (ep_dist == dist(y1, x2)):
+        x1[0] -= y1[0]
+        x1[1] -= y1[1]
+        y2[0] -= x2[0]
+        y2[1] -= x2[1]
+        v1, v2 = x1, y2
+    else:
+        x1[0] -= y1[0]
+        x1[1] -= y1[1]
+        x2[0] -= y2[0]
+        x2[1] -= y2[1]
+        v1, v2 = x1, x2
+    
+    # compute cos similarity
+    dot_prod = (v1[0] * v2[0]) + (v1[1] * v2[1])
+    cos_sim = dot_prod / (dist(v1, (0,0)) * dist(v2, (0,0)))
+
+    return np.arccos(cos_sim) < theta_thresh
+
+
+def remove_close_edges(edges, dist_thresh, theta_thresh):
+    '''
+    removes edges that are too close to one another
+    '''
+    n = len(edges)
+    adj_mat = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(i):
+            if (distance_less_thresh(edges[i], edges[j], dist_thresh, theta_thresh)):
+                adj_mat[i][j] = 1
+                adj_mat[j][i] = 1
+
+    return filter_edges(edges, adj_mat)
+
 
 def add_entries_in_radius(img, i, j, new_parent, radius, q):
     '''
@@ -167,11 +302,11 @@ def sample_edges_from_edges(img, min_length, radius=1):
             if (img_copy[i][j] != 0):
                 search(img_copy, i, j, min_length, edges, radius)
     
-    return edges
+    return remove_intersecting_edges(edges)
 
 if __name__ == '__main__':
     blurred, output = get_edges("images/portraits/abe.jpeg", 7, 7, 10, 150)
-    edges = sample_edges_from_edges(output, 45, radius=2)
+    edges = sample_edges_from_edges(output, 40, radius=1)
 
     width, height = output.shape
     image = np.ones((width, height)) * 255
