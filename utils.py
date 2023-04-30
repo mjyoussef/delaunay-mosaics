7cb9dc7f7a3fcf13ece507439a7e3afa1f7864d8
Michael Youssef <michael_youssef@brown.edu>
2023-04-30 00:51:43 -0400

    working search

--- utils.py
+++ utils.py
@@ -20,7 +20,6 @@ def get_edges(path, sigmaX, sigmaY, thresh1, thresh2):
     blurred = cv2.GaussianBlur(gray, (sigmaX, sigmaY), 0)
     output = cv2.Canny(blurred, thresh1, thresh2)
 
-    # cv2.imshow("blurred", blurred)
     # cv2.imshow("output", output)
     # cv2.waitKey(0)
 
@@ -53,59 +52,104 @@ def sample_points_from_edges(img, num_points):
 def dist(p1, p2):
     return np.sqrt( ((p1[0] - p2[0])**2) + ((p1[1] - p2[1])**2) )
 
-def search(img, i_start, j_start, min_length, max_length, edges):
+def orientation_of(p1, p2, p3):
+    '''
+    computes the orientation of three points (-1 = cw, 0 = colinear, 1 = ccw)
+    '''
+    det_matrix = np.array([[p1[0], p1[1], 1], [p2[0], p2[1], 1], [p3[0], p3[1], 1]])
+    det_sign = np.linalg.det(det_matrix)
+    if (det_sign < 0):
+        det_sign = -1
+    
+    if (det_sign > 0):
+        det_sign = 1
+
+    return det_sign
+
+def segments_intersect(e1, e2):
+    '''
+    returns true if two edges with no shared endpoints intersect each other
+    '''
+    if (e1[0] == e2[0] or e1[0] == e2[1] or e1[1] == e2[0] or e1[1] == e2[1]):
+        return False
+    
+    o_1 = orientation_of(e1[0], e1[1], e2[0])
+    o_2 = orientation_of(e1[0], e1[1], e2[1])
+
+    o_3 = orientation_of(e2[0], e2[1], e1[0])
+    o_4 = orientation_of(e2[0], e2[1], e1[0])
+
+    if (o_1 == 0): # colinear
+        x_projs = [(e1[0][0], 0), (e1[1][0], 0), (e2[0][0], 1), (e2[1][0], 1)]
+        list.sort(x_projs, key=lambda x: x[0])
+        y_projs = [(e1[0][1], 0), (e1[1][1], 0), (e2[0][1], 1), (e2[1][1], 1)]
+        list.sort(y_projs, key=lambda y: y[0])
+
+        return x_projs[0][1] != x_projs[1][1] or y_projs[0][1] != y_projs[0][1]
+        
+    return o_1 != o_2 and o_3 != o_4
+    
+def remove_intersecting_edges(edges):
+    '''
+    removes edges that intersect at anything other than end-points
+    '''
+    pass
+
+def remove_close_edges(edges):
+    '''
+    removes edges that are too close to one another;
+    '''
+
+def add_entries_in_radius(img, i, j, new_parent, radius, q):
+    '''
+    shuffles and adds elements from a (square) radius around i,j to the queue
+    '''
+
+    entries = []
+    for i_hat in range(max(0, i-radius), min(img.shape[0], i+radius+1)):
+        for j_hat in range(max(0, j-radius), min(img.shape[1], j+radius+1)):
+            entries.append((i_hat, j_hat, new_parent))
+    
+    random.shuffle(entries)
+
+    for entry in entries:
+        q.put(entry)
+
+def search(img, i_start, j_start, min_length, edges, radius):
+    '''
+    Helper method for `sample_edges_from_edges`
+    '''
     q = Queue()
 
-    # stores coordinate, reference to parent, # number of times an 
-    # option of adding a coordinate has been rejected
+    # stores coordinate, reference to parent
 
-    q.put((i_start, j_start, None, 0))
+    q.put((i_start, j_start, (i_start, j_start)))
 
     while (not q.empty()):
-        i, j, parent, rejected = q.get()
+        i, j, parent = q.get()
+
+        if (i < 0 or i >= img.shape[0] or j < 0 or j >= img.shape[1]):
+            continue
 
         if (img[i][j] == 0):
             continue
 
-
         new_parent = parent
-        new_rejected = rejected+1
 
         # check distance to parent
-        if (parent):
-            i2, j2 = parent
-            dist = dist((i, j), (i2, j2))
+        i2, j2 = parent
+        d = dist((i, j), (i2, j2))
 
-            if (dist > min_length):
-                # w/ probability 1 / (max_length - min_length - rejected)
-                # form a new edge between the parent and the current point
-                points_left = max_length - min_length - rejected
-                if (points_left == 0):
-                    edges.append(((i, j), (i2, j2)))
-                    new_parent = (i, j)
-                    new_rejected = 0
-                else:
-                    rand_num = random.uniform(0, 1)
-                    if (rand_num <= (1. / points_left)): # add edge
-                        edges.append(((i, j), (i2, j2)))
-                        new_parent = (i, j)
-                        new_rejected = 0
-            else:
-                if (max_length - min_length <= rejected):
-                    continue
+        if (d >= min_length):
+            edges.append(((i, j), (i2, j2)))
+            new_parent = (i, j)
         
         img[i][j] = 0
 
-        # add neighboring points to the queue
-        q.put((i-1, j, new_parent, new_rejected))
-        q.put((i+1, j, new_parent, new_rejected))
-        q.put((i, j-1, new_parent, new_rejected))
-        q.put((i, j+1, new_parent, new_rejected))
-
-    return edges
+        # add neighboring points (within a certain radius) to the queue
+        add_entries_in_radius(img, i, j, new_parent, radius, q)
         
-
-def sample_edges_from_edges(img, min_length, max_length):
+def sample_edges_from_edges(img, min_length, radius=1):
     '''
     Randomly samples edges that are bounded by certain length threshold
 
@@ -117,13 +161,23 @@ def sample_edges_from_edges(img, min_length, max_length):
     '''
 
     edges = []
-    img_copy = img.clone()
+    img_copy = img.copy()
     for i in range(img_copy.shape[0]):
         for j in range(img_copy.shape[1]):
             if (img_copy[i][j] != 0):
-                search(img_copy, i, j, min_length, max_length, edges)
+                search(img_copy, i, j, min_length, edges, radius)
     
     return edges
 
 if __name__ == '__main__':
-    get_edges("images/portraits/abe.jpeg", 7, 7, 30, 150)
+    blurred, output = get_edges("images/portraits/abe.jpeg", 7, 7, 10, 150)
+    edges = sample_edges_from_edges(output, 45, radius=2)
+
+    width, height = output.shape
+    image = np.ones((width, height)) * 255
+
+    for p1, p2 in edges:
+        cv2.line(image, (p1[1], p1[0]), (p2[1], p2[0]), (0, 255, 0), thickness=2)
+    
+    cv2.imshow("output", image)
+    cv2.waitKey(0)
