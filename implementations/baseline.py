b8998064bbae8c647ed0047286b33700571eb79e
Michael Youssef <michael_youssef@brown.edu>
2023-05-02 01:01:59 -0400

    partially completed baseline

--- implementations/baseline.py
+++ implementations/baseline.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from utils.sampling import randomly_sample_points, sparsify
+from utils.geo import Segment, Triangle
 
 '''
 Baseline Delaunay Triangulation...
@@ -17,11 +18,100 @@ Step 4: Save and display the mosaic from step 3
 '''
 
 def super_triangle(points):
-    pass
+    min_x = min_y = np.Inf
+    max_x = max_y = -np.Inf
+
+    for pt in points:
+        min_x = min(min_x, pt[0])
+        min_y = min(min_y, pt[1])
+        max_x = max(max_x, pt[0])
+        max_y = max(max_y, pt[1])
+
+    dx = (max_x - min_x) * 10
+    dy = (max_y - min_y) * 10
+
+    p0 = (min_x - dx, min_y - (dy*3))
+    p1 = (min_x - dx, max_y + dy)
+    p2 = (max_x + (3*dx), max_y + dy)
+
+    return Triangle(p0, p1, p2)
+
+def add_opposite_triangle(pt, t, stack, segments_to_triangle_dict):
+    opp_segment = t.opposite_segment(pt)
+    tris_w_opp_segment = segments_to_triangle_dict.get(opp_segment)
+
+    if (len(tris_w_opp_segment) > 1):
+        opposite_triangle = tris_w_opp_segment[0]
+        if (opposite_triangle == t):
+            opposite_triangle = tris_w_opp_segment[1]
+        
+        stack.append((opposite_triangle, opp_segment))
+
+def insert_point(pt, triangles, segments_to_triangle_dict):
+    init_t = None
+    stack = []
+
+    for t in triangles:
+        if (t.encloses(pt)):
+            init_t = t
+            break
+    
+    new_tri1 = Triangle(pt, init_t.A_coords, init_t.B_coords)
+    new_tri2 = Triangle(pt, init_t.A_coords, init_t.C_coords)
+    new_tri3 = Triangle(pt, init_t.B_coords, init_t.C_coords)
+
+    add_opposite_triangle(pt, new_tri1, stack, segments_to_triangle_dict)
+    add_opposite_triangle(pt, new_tri2, stack, segments_to_triangle_dict)
+    add_opposite_triangle(pt, new_tri3, stack, segments_to_triangle_dict)
+
+    while (len(stack) > 0):
+        t, opp_segment = stack.pop()
+        if (t.in_circumcircle(pt)): # swap diagonals of the convex quadrilateral
+
+            tris_w_opp_segment = segments_to_triangle_dict.get(opp_segment)
+            opp_tri_segments0 = tris_w_opp_segment[0].non_opposite_segments(pt, opp_segment)
+            opp_tri_segments1 = tris_w_opp_segment[1].non_opposite_segments(pt, opp_segment)
+
+            # find edges sharing similar endpoints from opp_segment
+            left, right = [], []
+            if (opp_tri_segments0[0].contains(opp_segment.p1)):
+                left.append(opp_tri_segments0[0])
+            else:
+                left.append(opp_tri_segments0[1])
+                right.append(opp_tri_segments0[0])
+            
+            if (opp_tri_segments1[0].contains(opp_segment.p1)):
+                left.append(opp_tri_segments1[0])
+            else:
+                left.append(opp_tri_segments1[1])
+                right.append(opp_tri_segments1[0])
+
+            # remove both triangles in `tris_w_opp_segment` from `triangles`
+            # dictionary update: remove `opp_segment` from dictionary and update values associated w/ segments from 
+            # `opp_tri_segments0` and `opp_tri_segments1`
+            
+
+            # add two new triangles to triangles
+            # dictionary update: update values associated with segments from `opp_tri_segments0` and `opp_tri_segments1`
+            # and add in the new diagonal segment
+
+            # update the stack using the two new triangles
+
 
 def triangulate(points):
-    pass
 
+    st = super_triangle(points)
+    triangles = [st]
+
+    # maps edges to corresponding triangles (2 max)
+    # note: useful for performing Lawson's search
+    segments_to_triangle_dict = {st.a: [st], st.b: [st], st.c: [st]}
+
+    for pt in points:
+        triangles = insert_point(pt, triangles, segments_to_triangle_dict)
+    
+    return triangles
+        
 
 
 ################### Arg parse function ######################
--- utils/geo.py
+++ utils/geo.py
@@ -71,3 +71,103 @@ def area_of(p1, p2, p3):
 
     s = 0.5 * (a + b + c)
     return np.sqrt(s * (s - a) * (s - b) + (s - c))
+
+
+############### Triangulation classes #######################
+class Segment():
+    def __init__(self, p1, p2):
+        pts = [p1, p2]
+        list.sort(pts)
+
+        self.p1 = pts[0]
+        self.p2 = pts[1]
+
+    def __hash__(self):
+        return str(self.p1) + " " + str(self.p2)
+    
+    def __eq__(self, other):
+        return isinstance(other, Segment) and self.p1 == other.p1 and self.p2 == other.p2
+    
+    def midpoint(self):
+        return ((self.p1[0] + self.p2[0]) / 2, (self.p1[1] + self.p2[1]) / 2)
+    
+    def contains(self, pt):
+        return self.p1 == pt or self.p2 == pt
+
+
+class Triangle():
+    def __init__(self, p1, p2, p3):
+
+        self.line = False
+        if (orientation_of(p1, p2, p3) == 0): # colinear
+            self.line = True
+        
+        self.a = Segment(p1, p2)
+        self.b = Segment(p1, p3)
+        self.c = Segment(p2, p3)
+
+        self.A_coords = p3
+        self.B_coords = p2
+        self.C_coords = p1
+
+        self.A, self.B, self.C, self.circumCenter = None, None, None, None
+
+        if (not self.line):
+            self.A = np.arccos(((self.b ** 2) + (self.c ** 2) - (self.a ** 2)) / (2 * self.b * self.c))
+            self.B = np.arccos(((self.a ** 2) + (self.c ** 2) - (self.b ** 2)) / (2 * self.a * self.c))
+            self.C = np.arccos(((self.a ** 2) + (self.b ** 2) - (self.c ** 2)) / (2 * self.a * self.b))
+
+            x = (self.A_coords[0] * np.sin(2*self.A)) + (self.B_coords[0] * np.sin(2*self.B)) + (self.C_coords[0] * np.sin(2*self.C))
+            x /= (np.sin(2*self.A) + np.sin(2*self.B) + np.sin(2*self.C))
+
+            y = (self.A_coords[1] * np.sin(2*self.A)) + (self.B_coords[1] * np.sin(2*self.B)) + (self.C_coords[1] * np.sin(2*self.C))
+            y /= (np.sin(2*self.A) + np.sin(2*self.B) + np.sin(2*self.C))
+
+            self.circumCenter = (x, y)
+    
+    def encloses(self, pt, ep=0.0005):
+        if (self.line):
+            raise Exception("Cannot check if a point is enclosed by a line")
+        
+        original_area = area_of(self.A_coords, self.B_coords, self.C_coords)
+        a1 = area_of(pt, self.A_coords, self.B_coords)
+        a2 = area_of(pt, self.A_coords, self.C_coords)
+        a3 = area_of(pt, self.B_coords, self.C_coords)
+        a_sum = a1 + a2 + a3
+
+        return (original_area <= a_sum + ep) and (original_area >= a_sum - ep)
+    
+    def in_circumcircle(self, pt, ep=0.001):
+        pt_dist = dist(pt, self.circumCenter)
+        radius = dist(self.A_coords, self.circumCenter)
+
+        return pt_dist <= radius + ep
+    
+    def __eq__(self, other):
+        pass
+    
+    def opposite_segment(self, pt):
+        if (not self.a.contains(pt)):
+            return self.a
+        elif (not self.b.contains(pt)):
+            return self.b
+        elif (not self.c.contains(pt)):
+            return self.c
+        else:
+            raise Exception("Input point is not a vertex of this triangle")
+        
+    def non_opposite_segments(self, opp_segment):
+        segments = []
+        if (self.a != opp_segment):
+            segments.append(self.a)
+        
+        if (self.b != opp_segment):
+            segments.append(self.b)
+
+        if (self.c != opp_segment):
+            segments.append(self.c)
+        
+        if (len(segments) >= 3):
+            raise Exception("\"Opposite segment\" is not part of this triangle")
+        
+        return segments
